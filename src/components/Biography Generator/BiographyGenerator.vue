b4b543863f237eab19d22df0ed70da49d8108e6d
eduolivag5 <eduolivag5@gmail.com>
2024-12-19 18:15:23 +0100

    changed biography generator availbaility

--- src/components/Biography Generator/BiographyGenerator.vue
+++ src/components/Biography Generator/BiographyGenerator.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="mb-4">
         <h1 class="text-white font-bold text-3xl md:text-5xl">Genera tu biografía</h1>
+
+        <div class="mt-4 rounded-lg bg-zinc-800 p-4 text-gray-300 text-sm">
+            <span>Esta página no está disponible actualmente.</span>
+        </div>
     </div>
 </template>
