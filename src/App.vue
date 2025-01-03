f1a7f21fadba13e157ac52afe9971715779f551b
eduolivag5 <eduolivag5@gmail.com>
2025-01-03 17:47:18 +0100

    fixed height screen issues

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
-  <div class="flex">
+  <div class="flex h-full">
     <div class="hidden md:block">
       <Sidebar />
     </div>
     
-    <div class="px-4 py-6 md:py-8 mx-auto w-full h-screen overflow-hidden max-w-4xl">
+    <div class="px-4 py-6 md:py-8 mx-auto w-full max-w-4xl">
       <router-view />
     </div>
-
-    
-
-    <!-- https://www.twitterbio.io/ -->
-    <!-- https://www.qrgpt.io/-->
   </div>
 </template>
 
